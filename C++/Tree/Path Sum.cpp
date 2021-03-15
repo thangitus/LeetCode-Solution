e0d3ecd7dca95848ae2e454909bd25da10bad8d2
thangitus <thang.itus@gmail.com>
2021-03-15 08:17:31 +0700

    Path sum

---
+++ C++/Tree/Path Sum.cpp
@@ -0,0 +1,23 @@
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+// https://leetcode.com/problems/path-sum/
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int targetSum) {
+        if (!root) return false;
+        targetSum -= root->val;
+        if (!(root->left || root->right || targetSum))
+            return true;
+        return hasPathSum(root->left, targetSum) || hasPathSum(root->right, targetSum);
+    }
+};
