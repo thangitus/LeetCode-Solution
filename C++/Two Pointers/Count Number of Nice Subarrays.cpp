b80d65947e3b25c24a99da1b569d7542a52f27a4
thangitus <thang.itus@gmail.com>
2021-04-07 11:21:22 +0700

    Count Number of Nice Subarrays - sol 2

--- C++/Two Pointers/Count Number of Nice Subarrays.cpp
+++ C++/Two Pointers/Count Number of Nice Subarrays.cpp
@@ -4,22 +4,26 @@ using namespace std;
 
 /*
  * https://leetcode.com/problems/count-number-of-nice-subarrays/
- * exactly(K) = atMost(K) - atMost(K-1)
+ * Nếu gặp số chẵn thì kệ cmn và cứ cộng thêm count
  */
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& A, int k) {
-        return atMost(A, k) - atMost(A, k - 1);
-    }
-
-    int atMost(vector<int>& A, int k) {
-        int res = 0, i = 0, n = A.size();
+    int numberOfSubarrays(vector<int> &A, int k) {
+        int res = 0, i = 0, count = 0, n = A.size();
         for (int j = 0; j < n; j++) {
-            k -= A[j] % 2;
-            while (k < 0)
-                k += A[i++] % 2;
-            res += j - i + 1;
+            if (A[j] & 1)
+                --k, count = 0;
+            while (k == 0)
+                k += A[i++] & 1, ++count;
+            res += count;
         }
         return res;
     }
 };
+
+int main() {
+    Solution solution;
+    vector<int> arr{1, 1, 2, 1, 1};
+    solution.numberOfSubarrays(arr, 1);
+    return 0;
+}
