886833aa54f6acc359adfc18a9f3577853d2bc6a
thangitus <thang.itus@gmail.com>
2021-06-08 10:18:49 +0700

    Building H2O

---
+++ Java-Kotlin/src/main/java/concurrency/H2O.java
@@ -0,0 +1,29 @@
+package concurrency;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ * https://leetcode.com/problems/building-h2o/
+ **/
+
+class H2O {
+
+    Semaphore h, o;
+
+    public H2O() {
+        h = new Semaphore(2, true);
+        o = new Semaphore(0, true);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        h.acquire();
+        releaseHydrogen.run();
+        o.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        o.acquire(2);
+        releaseOxygen.run();
+        h.release(2);
+    }
+}
