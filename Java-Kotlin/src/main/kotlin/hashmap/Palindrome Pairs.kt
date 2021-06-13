7b78ed26550410c00fb703cd935474f47b701789
thangitus <thang.itus@gmail.com>
2021-06-13 16:04:22 +0700

    Palindrome Pairs

--- Java-Kotlin/src/main/kotlin/hashmap/Palindrome Pairs.kt
+++ Java-Kotlin/src/main/kotlin/hashmap/Palindrome Pairs.kt
@@ -13,19 +13,27 @@ fun palindromePairs(words: Array<String>): List<List<Int>> {
 
     for (i in words.indices) {
         val cur = words[i]
+
+        // If contains reversed string of cur, then cur + cur.reversed() is palindrome.
         val reversed = cur.reversed()
         if (map.containsKey(reversed) && map[reversed] != i)
             res.add(listOf(i, map[reversed]!!))
+
+        // If curr is a palindrome string, then cur + "" and "" + cur are palindrome
         if (isPalindrome(cur, 0, cur.length - 1) && cur.isNotEmpty() && map.containsKey("")) {
             res.add(listOf(i, map[""]!!))
             res.add(listOf(map[""]!!, i))
         }
+
         for (len in 1 until cur.length) {
+            // If cur[0:len] is palindrome and there exists s2 is the reversing string of s1[len+1:] , then s2 + cur is palindrome
             if (isPalindrome(cur, 0, len - 1)) {
                 val rightSide = cur.subSequence(len, cur.length).reversed().toString()
                 if (map.containsKey(rightSide))
                     res.add(listOf(map[rightSide]!!, i))
             }
+
+            // If cur[len+1:] is palindrome and there exists s2 is the reversing string of s1[0:len] , then cur + s2 is palindrome.
             if (isPalindrome(cur, len, cur.length - 1)) {
                 val leftSide = cur.subSequence(0, len).reversed().toString()
                 if (map.containsKey(leftSide))
@@ -36,8 +44,8 @@ fun palindromePairs(words: Array<String>): List<List<Int>> {
     return res
 }
 
-private fun isPalindrome(s: String, left: Int, right: Int): Boolean {
-    var i = left
+private fun isPalindrome(s: String, start: Int, right: Int): Boolean {
+    var i = start
     var j = right
     while (i <= j)
         if (s[i++] != s[j--]) return false
