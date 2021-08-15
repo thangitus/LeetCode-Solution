7c28c093e567c603a5d0268df155b9b93688808d
thangitus <thang.itus@gmail.com>
2021-08-15 15:39:01 +0700

    Minimum Window Substring

--- C++/Two Pointers/Minimum Window Substring.cpp
+++ C++/Two Pointers/Minimum Window Substring.cpp
@@ -1,38 +1,30 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-/*
+
+/**
  * https://leetcode.com/problems/minimum-window-substring/
  * */
 
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char, int> countS, countT;
-        for (char &ch:t)
-            countT[ch]++;
-
-        int left, ansLeft, right, ansRight, length = INT_MAX;
-        left = right = 0;
-        for (; right < s.length(); right++) {
-            countS[s[right]]++;
-
-            while (check(countS, countT)) {
-                if (right - left + 1 < length) {
+        vector<int> countT(255);
+        for (char &ch:t)countT[ch]++;
+        int ansLeft, length = INT_MAX;
+        for (int right = 0, left = 0, count = 0; right < s.length(); right++) {
+            if (countT[s[right]] > 0) count++;
+            countT[s[right]]--;
+            if (count == t.length()) {
+                while (left < right && countT[s[left]] < 0) countT[s[left++]]++;
+                if (length > right - left + 1) {
                     ansLeft = left;
-                    ansRight = right;
                     length = right - left + 1;
                 }
-                countS[s[left++]]--;
+                countT[s[left++]]++;
+                count--;
             }
         }
-        return length == INT_MAX ? "" : s.substr(ansLeft, ansRight - ansLeft + 1);
-    }
-
-    bool check(unordered_map<char, int> &countS, unordered_map<char, int> &countT) {
-        for (auto &it : countT)
-            if (it.second > countS[it.first])
-                return false;
-        return true;
+        return length == INT_MAX ? "" : s.substr(ansLeft, length);
     }
 };
