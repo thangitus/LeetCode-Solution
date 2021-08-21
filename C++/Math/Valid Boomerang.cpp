6990e0d9f1de2fa3db392631a891d2c087f2c75e
thangitus <thang.itus@gmail.com>
2021-08-21 16:02:24 +0700

    Valid Boomerang

---
+++ C++/Math/Valid Boomerang.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/valid-boomerang/
+ * */
+
+class Solution {
+public:
+    bool isBoomerang(vector<vector<int>> &points) {
+        return points[0][0] * (points[1][1] - points[2][1]) + points[1][0] * (points[2][1] - points[0][1]) +
+               points[2][0] * (points[0][1] - points[1][1]) != 0;
+    }
+};
