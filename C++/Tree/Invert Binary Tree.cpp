874aeb6b1bd2352dcea551f4397d097edde6fe8d
thangitus <thang.itus@gmail.com>
2021-08-22 20:29:06 +0700

    Invert Binary Tree

---
+++ C++/Tree/Invert Binary Tree.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include "TreeNode.h"
+
+using namespace std;
+
+
+/**
+ * https://leetcode.com/problems/invert-binary-tree/
+ * */
+
+class Solution {
+public:
+    TreeNode *invertTree(TreeNode *root) {
+        if (!root) return root;
+        swap(root->left, root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+    }
+};
