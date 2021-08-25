2591820bff00b288d0f79be2d419ebd8b715c1ff
thangitus <thang.itus@gmail.com>
2021-08-25 22:08:07 +0700

    Greatest Common Divisor of Strings

---
+++ C++/String/Greatest Common Divisor of Strings.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/
+ * */
+
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        return (str1 + str2 == str2 + str1)
+               ? str1.substr(0, __gcd(str1.length(), str2.length()))
+               : "";
+    }
+};
