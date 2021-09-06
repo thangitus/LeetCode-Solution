84ff20a166862ccacda74f0f5460a5785e8600cc
thangitus <thang.itus@gmail.com>
2021-09-06 19:26:58 +0700

    Middle of the Linked List

---
+++ C++/Linked List/Middle of the Linked List.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include "ListNode.h"
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ */
+
+class Solution {
+public:
+    ListNode *middleNode(ListNode *head) {
+        ListNode *slow, *fast;
+        slow = fast = head;
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
