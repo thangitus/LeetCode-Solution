0576ba0eb7d0794cd39a3b09cf82761ced3e3390
thangitus <thang.itus@gmail.com>
2021-09-08 19:14:40 +0700

    Flood Fill

---
+++ C++/Depth-first Search/Flood Fill.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/flood-fill/
+ */
+
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>> &image, int row, int col, int newColor) {
+        if (image[row][col] == newColor) return image;
+        int oldColor = image[row][col];
+        image[row][col] = newColor;
+        if (row > 0 && image[row - 1][col] == oldColor)
+            floodFill(image, row - 1, col, newColor);
+
+        if (row < image.size() - 1 && image[row + 1][col] == oldColor)
+            floodFill(image, row + 1, col, newColor);
+
+        if (col > 0 && image[row][col - 1] == oldColor)
+            floodFill(image, row, col - 1, newColor);
+
+        if (col < image[0].size() - 1 && image[row][col + 1] == oldColor)
+            floodFill(image, row, col + 1, newColor);
+        return image;
+    }
+};
