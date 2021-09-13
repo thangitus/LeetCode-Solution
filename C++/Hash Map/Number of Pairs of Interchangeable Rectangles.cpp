84204a7a8499743230434686a46cadca15ba93c9
thangitus <thang.itus@gmail.com>
2021-09-13 08:54:37 +0700

    Number of Pairs of Interchangeable Rectangles

--- C++/Hash Map/Number of Pairs of Interchangeable Rectangles.cpp
+++ C++/Hash Map/Number of Pairs of Interchangeable Rectangles.cpp
@@ -2,17 +2,17 @@
 
 using namespace std;
 
+/**
+ * https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/
+ * */
 
 class Solution {
 public:
     long long interchangeableRectangles(vector<vector<int>> &rectangles) {
         unordered_map<double, int> maps;
         long long result = 0;
-        for (auto &rectangle:rectangles) {
-            double div = (double) rectangle[0] / rectangle[1];
-            result += maps[div];
-            maps[div]++;
-        }
+        for (auto &rectangle:rectangles)
+            result += maps[(double) rectangle[0] / rectangle[1]]++;
         return result;
     }
 };
