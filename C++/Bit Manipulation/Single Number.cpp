ba3fbf97ad4d75b39d3c3cdd5b5058cc60a15df3
thangitus <thang.itus@gmail.com>
2021-09-15 08:49:33 +0700

    Single Number

---
+++ C++/Bit Manipulation/Single Number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/single-number/
+ * */
+
+class Solution {
+public:
+    int singleNumber(vector<int> &nums) {
+        int result = 0;
+        for (int &num:nums)result ^= num;
+        return result;
+    }
+};
