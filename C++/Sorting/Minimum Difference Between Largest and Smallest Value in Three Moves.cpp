bf5eccb91e186a99ad7c1119162d2967d8a10d52
thangitus <thang.itus@gmail.com>
2021-09-17 22:01:08 +0700

    Minimum Difference Between Largest and Smallest Value in Three Moves

---
+++ C++/Sorting/Minimum Difference Between Largest and Smallest Value in Three Moves.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/minimum-difference-between-largest-and-smallest-value-in-three-moves/
+ * */
+
+class Solution {
+public:
+    int minDifference(vector<int> &nums) {
+        auto n = nums.size();
+        if (n <= 4) return 0;
+        sort(nums.begin(), nums.end());
+        return min({nums[n - 1] - nums[3], nums[n - 2] - nums[2], nums[n - 3] - nums[1], nums[n - 4] - nums[0]});
+    }
+};
