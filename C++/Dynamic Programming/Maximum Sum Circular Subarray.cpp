e0b8a0a41f55d7106db90f29d3abecf0764b3210
thangitus <thang.itus@gmail.com>
2021-09-20 17:33:29 +0700

    Maximum Sum Circular Subarray

--- C++/Dynamic Programming/Maximum Sum Circular Subarray.cpp
+++ C++/Dynamic Programming/Maximum Sum Circular Subarray.cpp
@@ -11,7 +11,7 @@ public:
     int maxSubarraySumCircular(vector<int> &nums) {
         int sum = 0, n = nums.size(), curr1, curr2, max_sum, min_sum;
         curr1 = curr2 = max_sum = min_sum = nums[0];
-        for (int i = 0; i < n; i++)sum += nums[i];
+        for (int i = 0; i < n; i++) sum += nums[i];
         for (int i = 1; i < n; i++) {
             curr1 = max(curr1 + nums[i], nums[i]);
             max_sum = max(curr1, max_sum);
