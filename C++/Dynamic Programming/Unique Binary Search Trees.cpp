2c7b4c5dcd724379383068764e045bc676b1befe
thangitus <thang.itus@gmail.com>
2021-09-25 11:40:46 +0700

    Unique Binary Search Trees (Update docs)

--- C++/Dynamic Programming/Unique Binary Search Trees.cpp
+++ C++/Dynamic Programming/Unique Binary Search Trees.cpp
@@ -5,6 +5,10 @@ using namespace std;
 /**
  * https://leetcode.com/problems/unique-binary-search-trees/
  * Refs: https://www.youtube.com/watch?v=OIc0mHgHUww
+ * numTree(4) =  numTree(0) * numTree(3) (choose 1 is root, so leftTree have 0 node, and rightTree have 3 nodes (2,3,4))
+ *             + numTree(1) * numTree(2) (choose 2 is root)
+ *             + numTree(2) * numTree(1) (choose 3 is root)
+ *             + numTree(3) * numTree(0) (choose 4 is root)
  * */
 
 class Solution {
