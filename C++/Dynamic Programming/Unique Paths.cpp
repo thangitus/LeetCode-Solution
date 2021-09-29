25f19cee44bb4137f143f5fbca44df36f72d5c12
thangitus <thang.itus@gmail.com>
2021-09-29 11:30:06 +0700

    Unique Paths

---
+++ C++/Dynamic Programming/Unique Paths.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/unique-paths/
+ * */
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if (i > 0)dp[i][j] += dp[i - 1][j];
+                if (j > 0)dp[i][j] += dp[i][j - 1];
+            }
+        return dp[m - 1][n - 1];
+    }
+};
