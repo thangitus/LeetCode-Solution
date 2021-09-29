47624122b6b23284b2ee4875a9b8e6b31bd3ea5a
thangitus <thang.itus@gmail.com>
2021-09-29 11:37:02 +0700

    Unique Paths II

--- C++/Dynamic Programming/Unique Paths II.cpp
+++ C++/Dynamic Programming/Unique Paths II.cpp
@@ -9,18 +9,15 @@ using namespace std;
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>> &obstacleGrid) {
-        int n = obstacleGrid.size(), m = obstacleGrid[0].size();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
-        dp[0][1] = 1;
-        for (int i = 0; i < n; i++)
-            for (int j = 0; j < m; j++) {
-                if (obstacleGrid[i][j]) {
-                    obstacleGrid[i][j] = 0;
-                    continue;
+        int m = obstacleGrid.size(), n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] = obstacleGrid[0][0] == 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++) {
+                if (!obstacleGrid[i][j]) {
+                    if (i > 0) dp[i][j] += dp[i - 1][j];
+                    if (j > 0) dp[i][j] += dp[i][j - 1];
                 }
-                if (i == 0 && j == 0) obstacleGrid[i][j] = 1;
-                if (i > 0) obstacleGrid[i][j] += obstacleGrid[i - 1][j];
-                if (j > 0)obstacleGrid[i][j] += obstacleGrid[i][j - 1];
             }
         return obstacleGrid.back().back();
     }
