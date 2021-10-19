78208532a86d76e66fd5b85cafa67366fb46e3d4
thangitus <thang.itus@gmail.com>
2021-10-19 22:05:33 +0700

    Number of Islands

---
+++ C++/Depth-first Search/Number of Islands.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/number-of-islands/
+ * */
+
+class Solution {
+public:
+    int dx[4] = {-1, 0, 0, 1};
+    int dy[4] = {0, -1, 1, 0};
+
+    int numIslands(vector<vector<char>> &grid) {
+        int result = 0;
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[0].size(); j++)
+                result += dfs(grid, i, j);
+        return result;
+    }
+
+    int dfs(vector<vector<char>> &grid, int x, int y) {
+        if (grid[x][y] == '0') return 0;
+        grid[x][y] = '0';
+        for (int i = 0; i < 4; i++) {
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if (newX < 0 || newX >= grid.size() || newY < 0 || newY >= grid[0].size()) continue;
+            dfs(grid, newX, newY);
+        }
+        return 1;
+    }
+};
