c011bc0ad2117dca4f8f7116f01bde04471aea5f
thangitus <thang.itus@gmail.com>
2021-10-20 08:24:41 +0700

    Longest Substring Without Repeating Characters

---
+++ C++/Sliding Window/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/**
+ * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+ * */
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int pos[255]{-1}, result = 0, left = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char chr = s[right];
+            if (pos[chr] != -1 && pos[chr] >= left)
+                left = pos[chr] + 1;
+            result = max(result, right - left + 1);
+            pos[chr] = right;
+        }
+        return result;
+    }
+};
