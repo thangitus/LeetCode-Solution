5555f6a36568c99101d910484369f9b526c26ed6
thangitus <thang.itus@gmail.com>
2021-12-13 11:49:38 +0700

    916. Word Subsets

---
+++ C++/Hash Map/916. Word Subsets.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string> &words1, vector<string> &words2) {
+        vector<int> count(26);
+        for (string &word: words2) {
+            auto tmp = counter(word);
+            for (int i = 0; i < 26; i++)
+                count[i] = max(count[i], tmp[i]);
+        }
+        vector<string> result;
+        for (string &word: words1) {
+            auto tmp = counter(word);
+            int i = 0;
+            for (; i < 26; i++)
+                if (tmp[i] < count[i])
+                    break;
+            if (i == 26) result.push_back(word);
+        }
+        return result;
+    }
+
+private:
+    vector<int> counter(string &word) {
+        vector<int> count(26);
+        for (char c: word) count[c - 'a']++;
+        return count;
+    }
+};
