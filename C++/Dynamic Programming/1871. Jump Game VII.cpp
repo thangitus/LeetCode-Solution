344e40105ab8f2eaf83cef20cf9343505654e676
Thang Nguyen <thang.itus@gmail.com>
2022-03-30 22:27:09 +0700

    1871. Jump Game VII

---
+++ C++/Dynamic Programming/1871. Jump Game VII.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canReach(string s, int minJ, int maxJ) {
+        int n = s.size(), pre = 0;
+        vector<bool> dp(n, false);
+        dp[0] = true;
+        for (int i = 1; i < n; ++i) {
+            if (i >= minJ && dp[i - minJ])
+                pre++;
+            if (i > maxJ && dp[i - maxJ - 1])
+                pre--;
+            dp[i] = pre > 0 && s[i] == '0';
+        }
+        return dp[n - 1];
+    }
+};
