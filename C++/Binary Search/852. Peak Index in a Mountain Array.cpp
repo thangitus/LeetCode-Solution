98655fbc614f77315b202a5da04cc0c554ab3d99
Thang Nguyen <thang.itus@gmail.com>
2022-04-28 21:35:55 +0700

    852. Peak Index in a Mountain Array

---
+++ C++/Binary Search/852. Peak Index in a Mountain Array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int> &nums) {
+        int left = 0, right = nums.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return left;
+    }
+};
