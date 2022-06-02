95a4d88ed8f6b261f233e00d2f43ebc0df68c8d1
Thang Nguyen <thang.itus@gmail.com>
2022-06-02 07:15:11 +0700

    867. Transpose Matrix

---
+++ C++/Array/867. Transpose Matrix.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>> &matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        vector<vector<int>> ans(m, vector<int>(n));
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < m; ++j)
+                ans[j][i] = matrix[i][j];
+        return ans;
+    }
+};
