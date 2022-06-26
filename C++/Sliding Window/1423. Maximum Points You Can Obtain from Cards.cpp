eed54818dce2a1df8cd70b0db3ba4d1e997ca163
Thang Nguyen <thang.itus@gmail.com>
2022-06-26 09:11:10 +0700

    1423. Maximum Points You Can Obtain from Cards

--- C++/Sliding Window/1423. Maximum Points You Can Obtain from Cards.cpp
+++ C++/Sliding Window/1423. Maximum Points You Can Obtain from Cards.cpp
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-/**
- * https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
- * */
-
 class Solution {
 public:
     int maxScore(vector<int> &cardPoints, int k) {
