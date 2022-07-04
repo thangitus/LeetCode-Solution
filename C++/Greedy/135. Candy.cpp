f99f4569cf0687cf4fbbe0d2884013a1456db34f
Thang Nguyen <thang.itus@gmail.com>
2022-07-04 20:26:22 +0700

    135. Candy

---
+++ C++/Greedy/135. Candy.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int candy(vector<int> &ratings) {
+        int n = ratings.size();
+        vector<int> count(n, 1);
+        for (int i = 1; i < n; ++i)
+            if (ratings[i] > ratings[i - 1])
+                count[i] = count[i - 1] + 1;
+        for (int i = n - 2; i >= 0; --i)
+            if (ratings[i] > ratings[i + 1])
+                count[i] = max(count[i], count[i + 1] + 1);
+        return accumulate(count.begin(), count.end(), 0);
+    }
+};
