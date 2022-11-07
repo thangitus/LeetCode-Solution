c09acee2b7d68d67a6bbe24bc7fd9f0ab808e1f7
Thang Nguyen <thang.itus@gmail.com>
2022-11-07 22:12:59 +0700

    1323. Maximum 69 Number

---
+++ C++/Greedy/1323. Maximum 69 Number.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int maximum69Number(int num) {
+        string s = to_string(num);
+        for (char &c: s)
+            if (c == '6') {
+                c = '9';
+                break;
+            }
+        return stoi(s);
+    }
+};
