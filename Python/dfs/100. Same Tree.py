786bb0ccb53c58e38a7a8276dd68b3edc98fd100
Thang Nguyen <thang.itus@gmail.com>
2023-01-10 07:30:48 +0700

    100. Same Tree

---
+++ Python/dfs/100. Same Tree.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from tree import TreeNode
+
+
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return p == q
