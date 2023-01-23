7112ebd121695bd6656c1d4aa501cc6d14ac68e8
Thang Nguyen <thang.itus@gmail.com>
2023-01-23 09:11:04 +0700

    997. Find the Town Judge

---
+++ Python/array/997. Find the Town Judge.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        count = [0] * (n + 1)
+        for a, b in trust:
+            count[b] += 1
+            count[b] -= 1
+        for i in range(1, n + 1):
+            if count[i] == n - 1: return i
+        return -1
