37813bdc09846fd6a50174596d2ddbecb08d793e
Thang Nguyen <thang.itus@gmail.com>
2023-02-06 07:19:04 +0700

    1470. Shuffle the Array

---
+++ Python/array/1470. Shuffle the Array.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i + n])
+        return res
