63f365aa677ba295757ee5142035957b0472d5f1
Thang Nguyen <thang.itus@gmail.com>
2023-03-19 20:26:20 +0700

    211. Design Add and Search Words Data Structure (Shorter)

--- Python/trie/211. Design Add and Search Words Data Structure.py
+++ Python/trie/211. Design Add and Search Words Data Structure.py
@@ -1,31 +1,17 @@
 class WordDictionary:
-    class TrieNode:
-        def __init__(self):
-            self.children = {}
-            self.endOfWord = False
-
-    def search_trie(self, cur, i, word):
-        if i == len(word): return cur.endOfWord
-        if word[i] != '.':
-            if word[i] not in cur.children:
-                return False
-            return self.search_trie(cur.children[word[i]], i + 1, word)
-
-        if word[i] == '.':
-            for child in cur.children.values():
-                if self.search_trie(child, i + 1, word): return True
-        return False
-
     def __init__(self):
-        self.root = self.TrieNode()
+        self.root = {}
 
     def addWord(self, word: str) -> None:
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = self.TrieNode()
-            cur = cur.children[c]
-        cur.endOfWord = True
+        node = self.root
+        for char in word:
+            node = node.setdefault(char, {})
+        node['$'] = True
 
     def search(self, word: str) -> bool:
-        return self.search_trie(self.root, 0, word)
+        def dfs(node, i):
+            if i == len(word): return '$' in node
+            if word[i] == '.': return any(dfs(child, i + 1) for child in node.values() if isinstance(child, dict))
+            return word[i] in node and dfs(node[word[i]], i + 1)
+
+        return dfs(self.root, 0)
