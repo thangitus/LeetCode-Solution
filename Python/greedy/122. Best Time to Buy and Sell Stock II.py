7ca2cbc7dffa961d4423ee79c7dbb8230b7ad9f1
Thang Nguyen <thang.itus@gmail.com>
2024-03-06 19:49:11 +0700

    122. Best Time to Buy and Sell Stock II

---
+++ Python/greedy/122. Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,6 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(1, len(prices)):
+            profit += max(0, prices[i] - prices[i - 1])
+        return profit
