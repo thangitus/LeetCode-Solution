fa2e17ccaa6ab35dbda5c550cb03149757d896eb
Thang Nguyen <thang.itus@gmail.com>
2024-03-24 09:49:47 +0700

    287. Find the Duplicate Number

---
+++ Python/two pointers/287. Find the Duplicate Number.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = nums[0], nums[nums[0]]
+        while slow != fast:
+            slow, fast = nums[slow], nums[nums[fast]]
+        slow = 0
+        while slow != fast:
+            slow, fast = nums[slow], nums[fast]
+        return slow
+
+
