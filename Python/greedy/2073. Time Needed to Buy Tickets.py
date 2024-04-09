5c68a521359aeda8dcebc5ce745c560645098626
Thang Nguyen <thang.itus@gmail.com>
2024-04-09 22:04:51 +0700

    2073. Time Needed to Buy Tickets

---
+++ Python/greedy/2073. Time Needed to Buy Tickets.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                time += min(tickets[i], tickets[k])
+            else:
+                time += min(tickets[k] - 1, tickets[i])
+        return time
